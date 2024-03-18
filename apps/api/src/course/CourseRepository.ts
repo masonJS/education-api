ce4539dc1cbd86edcca50035e1307089f3eb88b1
minwoo <mwoodev526@gmail.com>
2024-03-18 19:23:41 +0900

    feat. 강의 목록 조회 repository 구현

--- apps/api/src/course/CourseRepository.ts
+++ apps/api/src/course/CourseRepository.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Course } from '@app/entity/domain/course/Course.entity';
 import { Repository } from 'typeorm';
+import { CourseStatus } from '@app/entity/domain/course/type/enum/CourseStatus';
+import { CourseSearchCondition } from './service/dto/CourseSearchCondition';
 
 @Injectable()
 export class CourseRepository {
@@ -14,5 +16,36 @@ export class CourseRepository {
     return await this.repository.findOneBy({
       title,
     });
+  }
+
+  async findAll(condition: CourseSearchCondition) {
+    const queryBuilder = this.repository
+      .createQueryBuilder('course')
+      .select(['course', 'instructor.name'])
+      .innerJoin('course.instructor', 'instructor')
+      .where('course.status = :status', { status: CourseStatus.PUBLISH });
+
+    if (condition.hasCategory) {
+      queryBuilder.andWhere('course.category = :category', {
+        category: condition.category,
+      });
+    }
+
+    if (condition.hasInstructorName) {
+      queryBuilder.andWhere('instructor.name = :instructorName', {
+        instructorName: condition.searchKeyword,
+      });
+    }
+
+    if (condition.hasCourseTitle) {
+      queryBuilder.andWhere('course.title = :courseTitle', {
+        courseTitle: condition.searchKeyword,
+      });
+    }
+
+    return queryBuilder
+      .offset(condition.offset)
+      .limit(condition.limit)
+      .getMany();
   }
 }
--- apps/api/test/integration/course/CourseRepository.spec.ts
+++ apps/api/test/integration/course/CourseRepository.spec.ts
@@ -2,13 +2,19 @@ import { Test } from '@nestjs/testing';
 import { getRealDBModule } from '@app/entity/getRealDBModule';
 import { EntityModule } from '@app/entity/EntityModule';
 import { DataSource } from 'typeorm';
+import { CourseStatus } from '@app/entity/domain/course/type/enum/CourseStatus';
+import { CourseCategory } from '@app/entity/domain/course/type/enum/CourseCategory';
 import { CourseRepository } from '../../../src/course/CourseRepository';
 import { CourseFactory } from '../../../../../libs/entity/test/factory/CourseFactory';
 import { expectNonNullable } from '../../../../../libs/web-common/test/util/expect/expectNonNullable';
+import { CourseSearchCondition } from '../../../src/course/service/dto/CourseSearchCondition';
+import { InstructorFactory } from '../../../../../libs/entity/test/factory/InstructorFactory';
+import { CourseSearchCategory } from '../../../src/course/type/enum/CourseSearchCategory';
 
 describe('CourseRepository', () => {
   let courseRepository: CourseRepository;
   let courseFactory: CourseFactory;
+  let instructorFactory: InstructorFactory;
   let dataSource: DataSource;
 
   beforeAll(async () => {
@@ -21,6 +27,7 @@ describe('CourseRepository', () => {
 
     courseRepository = module.get(CourseRepository);
     courseFactory = new CourseFactory(dataSource.createEntityManager());
+    instructorFactory = new InstructorFactory(dataSource.createEntityManager());
   });
 
   beforeEach(async () => courseFactory.clear());
@@ -46,6 +53,144 @@ describe('CourseRepository', () => {
       // then
       expectNonNullable(result);
       expect(result.title).toBe(course.title);
+    });
+  });
+
+  describe('findAll', () => {
+    it('조회되는 강의가 없을시 빈 배열을 반환한다.', async () => {
+      // given
+      const condition = new CourseSearchCondition(0, 10);
+
+      // when
+      const result = await courseRepository.findAll(condition);
+
+      // then
+      expect(result).toHaveLength(0);
+    });
+
+    it('강의 목록을 조회한다.', async () => {
+      // given
+      const condition = new CourseSearchCondition(0, 10);
+      const instructor = await instructorFactory.save();
+      const course = await courseFactory.save({
+        instructor,
+        status: CourseStatus.PUBLISH,
+      });
+
+      // when
+      const result = await courseRepository.findAll(condition);
+
+      // then
+      expect(result).toHaveLength(1);
+      expect(result[0].title).toBe(course.title);
+      expect(result[0].instructorName).toBe(instructor.name);
+    });
+
+    it('카테고리 기준으로 강의 목록을 조회한다.', async () => {
+      // given
+      const searchCategory = CourseCategory.DATA;
+      const condition = new CourseSearchCondition(
+        0,
+        10,
+        undefined,
+        undefined,
+        searchCategory,
+      );
+      const instructor = await instructorFactory.save();
+      const course = await courseFactory.save({
+        instructor,
+        status: CourseStatus.PUBLISH,
+        category: searchCategory,
+      });
+      await courseFactory.save({
+        instructor,
+        status: CourseStatus.PUBLISH,
+        category: CourseCategory.APP,
+      });
+
+      // when
+      const result = await courseRepository.findAll(condition);
+
+      // then
+      expect(result).toHaveLength(1);
+      expect(result[0].title).toBe(course.title);
+    });
+
+    it('강의 명 기준으로 강의 목록을 조회한다.', async () => {
+      // given
+      const searchKeyword = 'course_title';
+      const condition = new CourseSearchCondition(
+        0,
+        10,
+        searchKeyword,
+        CourseSearchCategory.COURSE_TITLE,
+      );
+      const instructor = await instructorFactory.save();
+      const course = await courseFactory.save({
+        instructor,
+        status: CourseStatus.PUBLISH,
+        title: searchKeyword,
+      });
+      await courseFactory.save({
+        instructor,
+        status: CourseStatus.PUBLISH,
+      });
+
+      // when
+      const result = await courseRepository.findAll(condition);
+
+      // then
+      expect(result).toHaveLength(1);
+      expect(result[0].title).toBe(course.title);
+    });
+
+    it('강사 명 기준으로 강의 목록을 조회한다.', async () => {
+      // given
+      const searchKeyword = 'instructor_name';
+      const condition = new CourseSearchCondition(
+        0,
+        10,
+        searchKeyword,
+        CourseSearchCategory.INSTRUCTOR_NAME,
+      );
+      const course = await courseFactory.save({
+        instructor: await instructorFactory.save({
+          name: searchKeyword,
+        }),
+        status: CourseStatus.PUBLISH,
+      });
+      await courseFactory.save({
+        instructor: await instructorFactory.save(),
+        status: CourseStatus.PUBLISH,
+      });
+
+      // when
+      const result = await courseRepository.findAll(condition);
+
+      // then
+      expect(result).toHaveLength(1);
+      expect(result[0].title).toBe(course.title);
+    });
+
+    it('강의 목록을 페이징 단위로 페이지네이션을 한다.', async () => {
+      // given
+      const condition = new CourseSearchCondition(0, 1);
+      const instructor = await instructorFactory.save();
+      const course = await courseFactory.save({
+        instructor,
+        status: CourseStatus.PUBLISH,
+      });
+      await courseFactory.save({
+        instructor,
+        status: CourseStatus.PUBLISH,
+      });
+
+      // when
+      const result = await courseRepository.findAll(condition);
+
+      // then
+      expect(result).toHaveLength(1);
+      expect(result[0].title).toBe(course.title);
     });
   });
 });
